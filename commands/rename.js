6ad17af63a2ec3fa39e13a226fbf72430b7da7d2
Tupperbox <support@tupperbox.app>
2019-09-17 13:49:27 -0700

    add name limit QOL to rename command too

--- commands/rename.js
+++ commands/rename.js
@@ -13,7 +13,7 @@ module.exports = {
 		let member = await bot.db.getMember(msg.author.id,args[0]);
 		if(!args[1]) return "Missing argument 'newname'.";
 		let newMember = await bot.db.getMember(msg.author.id,args[1]);
-		if(args[1].length < 2 || args[1].length > 76) return "New name must be between 2 and 76 characters.";
+		if(args[1].length < 1 || args[1].length > 76) return "New name must be between 1 and 76 characters.";
 		if(!member) return "You don't have " + article(cfg) + " " + cfg.lang + " with that name registered.";
 		if(newMember) return "You already have " + article(cfg) + " " + cfg.lang + " with that new name.";
 		
