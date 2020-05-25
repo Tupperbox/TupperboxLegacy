8c34e796dcc58af44f77214dcff192a2921bee63
Tupperbox <support@tupperbox.app>
2020-05-24 18:28:30 -0600

    remove ancient reference to old logger

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = bot => {  
-	bot.logger.info(`Connected\nLogged in as:\n${bot.user.username} - (${bot.user.id})`);
+	//bot.logger.info(`Connected\nLogged in as:\n${bot.user.username} - (${bot.user.id})`);
 	bot.updateStatus();
 	setInterval(() => bot.updateStatus(), 1800000);
 };
