4475c6899e2a9eeab85993ab74ad579ab559b7d5
Tupperbox <support@tupperbox.app>
2020-07-11 23:11:29 -0600

    small cleanup

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,4 @@
 module.exports = bot => {  
-	//bot.logger.info(`Connected\nLogged in as:\n${bot.user.username} - (${bot.user.id})`);
 	bot.updateStatus();
 	setInterval(() => bot.updateStatus(), 1800000);
 };
