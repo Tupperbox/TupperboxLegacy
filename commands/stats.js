46fd3d82e4ab51b6eeae7838d6c9545ea03e433f
Tupperbox <support@tupperbox.app>
2020-08-05 14:03:19 -0600

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -6,6 +6,6 @@ module.exports = {
 	desc: cfg =>  "Displays a list of useful technical information about the bot's running processes. Lists technical details of all clusters, useful for monitoring recent outages and the progress of ongoing restarts. Displays the total memory usage and allocation of the bot, along with how many servers the bot is serving. Displays which shard is currently supporting this server and which cluster that shard is a part of.",
 	permitted: msg => true,
 	execute: (bot, msg, args, cfg) => {
-		process.send({name: "postStats", channelID: msg.channel.id, shard: msg.channel.guild ? msg.channel.guild.shard.id : 0});
+		process.send({name: "postStats", channelID: msg.channel.id, shard: msg.channel.guild ? msg.channel.guild.shard.id : 0, cluster: bot.base.clusterID});
 	}
 };
