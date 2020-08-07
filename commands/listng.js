05d563300c6ff4c7c5c8d0d04bbebe7146f339f7
Tupperbox <support@tupperbox.app>
2020-08-07 17:50:09 -0600

    add cooldown to listng

--- commands/listng.js
+++ commands/listng.js
@@ -2,6 +2,7 @@ module.exports = {
 	help: cfg => "Like list, but without showing group info.",
 	usage: cfg =>  ["list [user] - Sends a list of the user's registered " + cfg.lang + "s, their brackets, post count, and birthday (if set). If user is not specified it defaults to the message author.\nThe bot will provide reaction emoji controls for navigating long lists: Arrows navigate through pages, # jumps to a specific page, ABCD jumps to a specific " + cfg.lang + ", and the stop button deletes the message."],
 	permitted: () => true,
+	cooldown: msg => 60000,
 	execute: async (bot, msg, args, cfg, members) => {
 		return bot.cmds.list.execute(bot,msg,args,cfg,members,true);
 	}
