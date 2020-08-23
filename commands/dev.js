2d1378e6685fe954af6564f084aa394f8dfd4490
dev-kittens <acw@ml1.net>
2020-08-23 03:36:13 -0400

    Refactor dev command(s)

---
+++ commands/dev.js
@@ -0,0 +1,23 @@
+const util = require("util");
+
+module.exports = {
+    permitted: (msg) => msg.author.id == bot.owner,
+    execute: async (bot, msg, args, cfg) => {
+        switch(args.shift()) {
+            case "eval":
+                let out;
+                try {
+                    out = await eval(msg.content.slice(cfg.prefix.length + 9).trim());
+                } catch(e) { out = e.toString(); }
+                return util.inspect(out).slice(0,2000);
+            case "reload":
+                process.send({name: "broadcast", msg: {name: "reload", type: args[0], targets: args.slice(1), channel: msg.channel.id}});
+                if(args[0] == "queue") process.send({name:"reloadQueue"});
+                if(args[0] == "ipc") process.send({name:"reloadIPC"});
+                break;
+            case "blacklist":
+                await bot.banAbusiveUser(args.shift(), msg.channel.id);
+                break;
+            }
+    }
+}
