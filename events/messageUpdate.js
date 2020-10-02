3cfd5c3676660b55d799b11ad6c5dac7bf541e66
Tupperbox <support@tupperbox.app>
2020-10-01 22:17:58 -0600

    Create main.yml

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,7 +1,7 @@
 module.exports = async (msg,_,bot) => {
 	// occasionally errors on bot message embeds for some reason?
 	if (!msg.author) return;
-        // ignore messages sent more than 10 minutes ago
-        if (Date.now() - msg.timestamp > 1000*60*10) return;
-        bot.msg(bot, msg, true);
+	// ignore messages sent more than 10 minutes ago
+	if (Date.now() - msg.timestamp > 1000*60*10) return;
+	bot.msg(bot, msg, true);
 };
