54c7a3b255ba88455ff8d7d39cfc3fca0372637d
kittens <acw@ml1.net>
2020-10-04 11:26:36 -0400

    Don't show an unhandled rejection error

--- commands/privacy.js
+++ commands/privacy.js
@@ -1,6 +1,6 @@
 const fs = require("fs").promises;
 let policy = null;
-fs.readFile("./privacy.txt").then(file => policy = file.toString());
+fs.readFile("./privacy.txt").then(file => policy = file.toString()).catch((err) => { if(err.code != "ENOENT") console.warn(err); });
 
 module.exports = {
 	help: cfg => "View my privacy policy.",
